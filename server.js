f1597e809ea006fc4e26fb1ab21a30040ca8dcd5
niharika5112004 <niharika.dusane@gmail.com>
2026-04-09 17:56:49 +0530

    fix ssl connection

--- server.js
+++ server.js
@@ -12,9 +12,7 @@ app.use(express.json());
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
 function authenticateToken(req, res, next) {
